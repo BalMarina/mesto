22443486a79f1a53ba34683bcc85abb7f6de1b86
Marina Balyasova <marie.kopaeva@gmail.com>
2022-05-24 00:10:30 +0300

    fix: написать функцию обработки закрытия попапов с формами

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,17 +1,24 @@
-const popupCard = document.querySelector('.popup-card');
-
 function openPopup(popup) {
   popup.classList.add('popup_opened');
   document.addEventListener('keyup', closePopupOnEsc);
 };
 
+function openPopupWithForm(popup) {
+  popup.classList.add('popup_opened');
+  document.addEventListener('keyup', closePopupWithFormOnEsc);
+};
+
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
   document.removeEventListener('keyup', closePopupOnEsc);
+};
+
+function closePopupWithForm(popup) {
   const popupForm = popup.querySelector('.popup__form');
   if (popupForm) {
     popupForm.reset();
-  };
+  }
+  closePopup(popup);
 };
 
 function closePopupOnEsc(e) {
@@ -21,4 +28,25 @@ function closePopupOnEsc(e) {
   }
 };
 
-export { popupCard, openPopup, closePopup };
+function closePopupWithFormOnEsc(e) {
+  if (e.key == 'Escape') {
+    const openedPopup = document.querySelector('.popup_opened');
+    closePopupWithForm(openedPopup);
+  }
+};
+
+function setPopupCloseHandler(popup, closeHandler) {
+  const popupCloseBtn = popup.querySelector('.popup__close-icon');
+
+  popup.addEventListener('click', function (e) {
+    if (e.target === e.currentTarget) {
+      closeHandler(popup);
+    };
+  });
+
+  popupCloseBtn.addEventListener('click', function (e) {
+    closeHandler(popup);
+  });
+}
+
+export { openPopup, openPopupWithForm, closePopup, closePopupWithForm, setPopupCloseHandler };
