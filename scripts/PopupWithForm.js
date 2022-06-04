35d1e0e04b2025899d04273c8a7317ca25fbb5e6
Marina Balyasova <marie.kopaeva@gmail.com>
2022-06-04 15:53:53 +0300

    feat: добавить класс PopupWithForm

---
+++ scripts/PopupWithForm.js
@@ -0,0 +1,33 @@
+import Popup from '.Popup.js'
+
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super(popupSelector)
+    this._handleFormSubmit = handleFormSubmit
+    this._form = form
+  }
+
+  _getInputValues() {
+    this._inputList = Array.from(this._popup.querySelectorAll('.popup__input'))
+    this._inputValues = {};
+    this._inputList.forEach((input) => {
+
+      this._inputValues[input.name] = input.value;
+    });
+    return this._inputList
+  }
+
+  setEventListeners(e) {
+    e.preventDefault()
+    super.setEventListeners()
+    this._form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      this._handleFormSubmit(this._getInputValues())
+    })
+  }
+
+  close() {
+    this._popupForm.reset();
+    super.close();
+  }
+}
